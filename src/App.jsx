166a6810590a0fda0f48a26462d9dca1fc6c203c
axelrammon <axelrammon48@gmail.com>
2024-03-30 15:01:28 -0300

    adicionando form

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-// import { logotipoToDo } from './assets/logo.svg';
 import styles from './App.module.css'
 import { Header } from './components/Header';
+import { Form } from './components/Form';
 
 import './global.css';
 
 function App() {
   return (
     <div className={styles.teste}>
-     <Header />
-
+      <Header />
+      <Form />
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,17 @@
-
+import { PlusCircle } from "phosphor-react";
+import styles from './Form.module.css';
 
 export function Form() {
   return(
     <>
-      
+      <div className={styles.form}>
+        <div>
+          <input className={styles.inputTask} type="text" placeholder="Adicione uma nova tarefa" />
+        </div>
+        <div>
+          <button className={styles.buttonCriarTask}>Criar <PlusCircle size={16} /></button>
+        </div>
+      </div>
     </>
   );
 }
